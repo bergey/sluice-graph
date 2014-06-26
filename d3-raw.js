3590bd5755e5abfc9670eafce9eba5b35f5f986d
Daniel Bergey <bergey@alum.mit.edu>
2014-06-26 18:13:58 +0000

    Move js into separate file

---
+++ d3-raw.js
@@ -0,0 +1,69 @@
+/* global d3 */
+
+"use strict";
+
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var x = d3.scale.linear()
+    .range([0, width]);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.csv("Benchmarking_Data_Public.csv", function(error, data) {
+    data.forEach(function(d) {
+        d.eui = +d.SITE_EUI;
+        d.floorA = +d.BLDG_FLOOR_AREA;
+    });
+
+  x.domain(d3.extent(data, function(d) { return d.floorA; })).nice();
+  y.domain(d3.extent(data, function(d) { return d.eui; })).nice();
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+    .append("text")
+      .attr("class", "label")
+      .attr("x", width)
+      .attr("y", -6)
+      .style("text-anchor", "end")
+      .text("Floor Area [sf]");
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("class", "label")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Energy Use Intensity [kBTU/yr/sf]");
+
+  svg.selectAll(".dot")
+      .data(data)
+    .enter().append("circle")
+      .attr("class", "dot")
+      .attr("r", 2)
+      .attr("cx", function(d) { return x(d.floorA); })
+      .attr("cy", function(d) { return y(d.eui); })
+      .style("fill", "blue");
+
+});
