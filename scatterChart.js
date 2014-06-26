4f3e9511213e2adef1a082c8eddf915214a24627
Daniel Bergey <bergey@alum.mit.edu>
2014-06-26 10:49:58 -0400

    semicolons

--- scatterChart.js
+++ scatterChart.js
@@ -1,14 +1,14 @@
 function show_details(d) {
     var div = d3.select('#selected');
     div.html("");
-    div.append("h3").text(d.FACILITY_DESCRIPTION)
+    div.append("h3").text(d.FACILITY_DESCRIPTION);
     div.append('p').html(d.ASSET_ADDR + "<br />" + "Philadelphia, PA");
-    var table = div.append('table')
+    var table = div.append('table');
     table_row(table, 'Build in', d.YEAR_BUILT);
     table_row(table, 'Floor Area', d.BLDG_FLOOR_AREA + ' square feet');
     table_row(table, 'Electricity', d.ELECTRIC_USE + ' kWh per year');
     table_row(table, 'Gas', d.GAS_USE);
-};
+}
 
 function table_row(table, key, value) {
     table.append('tr').html('<td><strong>' + key + '</strong><td>' + value);
