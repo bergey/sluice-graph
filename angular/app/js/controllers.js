e7bb9b51144fc0d1eb2e5903a5bc9c5af18cc423
Daniel Bergey <bergey@alum.mit.edu>
2014-06-27 23:07:56 +0000

    Angular version
    input elements tied to Angular model
    custom Angular directive to draw graph with D3
    
    Only sortof works

--- angular/app/js/controllers.js
+++ angular/app/js/controllers.js
@@ -1,9 +1,37 @@
+/* global _ */
+
 'use strict';
 
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+    .controller('BuildingList', ['$scope', '$http', 
+                                 function($scope, $http) {
+                                     $http.get('Benchmarking_Data_Public.json').success(function(data) {
+                                         // $scope.buildings=_.map(data, function(d) {
+                                         //     d.eui = +d.SITE_EUI;
+                                         //     d.sf = d.BLDG_FLOOR_AREA;
+                                         //     return d;
+                                         // });
+                                         $scope.buildings = data;
+                                         $scope.sectors = {};
+                                         _.each(data, function(d) {
+                                             if (d.SECTOR != "" && ! _.has($scope.sectors, d.SECTOR)) {
+                                                 $scope.sectors[d.SECTOR] = true;
+                                             }
+                                        });
+                                         $scope.sfMin = _.min(_.map(data, function(d) {return d.BLDG_FLOOR_AREA;}));
+                                         $scope.sfGraphMin = $scope.sfMin;
+                                         $scope.sfMax = _.max(_.map(data, function(d) {return d.BLDG_FLOOR_AREA;}));
+                                         $scope.sfGraphMax = $scope.sfMax;
+                                         $scope.filterBldgs = function(sfMin, sfMax, sectors) {
+                                             return function(d) { 
+                                                 return sfMin < d.BLDG_FLOOR_AREA && d.BLDG_FLOOR_AREA < sfMax && sectors[d.SECTOR]; 
+                                             }
+                                         };
+                                     });
+                                 }])
+    .controller('MyCtrl1', ['$scope', function($scope) {
 
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
--- angular/app/js/directives.js
+++ angular/app/js/directives.js
@@ -3,9 +3,24 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
+angular.module('myApp.directives', [])
+  .directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]);
+  }])
+    .directive('ngd3Scatter', function() {
+        return {
+            'restrict': 'A',
+            'scope': {
+                datajson: '=',
+            },
+            'link': function(scope, elem, attrs) {
+                // TODO understand actual calling convention here
+                // Should this work on every element?
+                var plot = scatterplot(scope).xKey(attrs.xkey).yKey(attrs.ykey).xAxisLabel(attrs.xaxislabel).yAxisLabel(attrs.yaxislabel);
+                plot.element('#'+elem[0].id);
+                plot();
+            }
+        };
+    });
