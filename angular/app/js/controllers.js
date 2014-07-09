2715bb36b77c441fb63e280e4b20248cb7ae1f6a
Daniel Bergey <bergey@alum.mit.edu>
2014-07-09 15:48:59 +0000

    WIP: add margins

--- angular/app/js/controllers.js
+++ angular/app/js/controllers.js
@@ -8,18 +8,15 @@ angular.module('myApp.controllers', [])
     .controller('BuildingList', ['$scope', '$http', 
                                  function($scope, $http) {
                                      $http.get('Benchmarking_Data_Public.json').success(function(data) {
-                                         // $scope.buildings=_.map(data, function(d) {
-                                         //     d.eui = +d.SITE_EUI;
-                                         //     d.sf = d.BLDG_FLOOR_AREA;
-                                         //     return d;
-                                         // });
                                          $scope.buildings = data;
+                                         // populate sectors checkboxes with fields appearing in data
                                          $scope.sectors = {};
                                          _.each(data, function(d) {
                                              if (d.SECTOR !== "" && ! _.has($scope.sectors, d.SECTOR)) {
                                                  $scope.sectors[d.SECTOR] = true;
                                              }
                                         });
+                                         // populate square footage fields with range of data
                                          $scope.sfMin = _.min(_.map(data, function(d) {return d.BLDG_FLOOR_AREA;}));
                                          $scope.sfGraphMin = $scope.sfMin;
                                          $scope.sfMax = _.max(_.map(data, function(d) {return d.BLDG_FLOOR_AREA;}));
@@ -31,8 +28,12 @@ angular.module('myApp.controllers', [])
                                          };
                                          $scope.width = 800;
                                          $scope.height = 400;
-                                         var x = d3.scale.linear().range([0,$scope.width]).domain([$scope.sfMin, $scope.sfMax]);
-                                         var y = d3.scale.linear().range([$scope.height,0]).domain(d3.extent(data, _.property("SITE_EUI")));
+                                         $scope.marginLeft = 25;
+                                         $scope.marginBottom = 25;
+                                         $scope.marginRight = 25;
+                                         $scope.marginTop = 25;
+                                         var x = d3.scale.linear().range([0,$scope.width-$scope.marginLeft-$scope.marginRight]).domain([$scope.sfMin, $scope.sfMax]);
+                                         var y = d3.scale.linear().range([$scope.height-$scope.marginTop-$scope.marginBottom,0]).domain(d3.extent(data, _.property("SITE_EUI")));
                                          $scope.points = _.map(data, function(d) {
                                              return { "x": x(d.BLDG_FLOOR_AREA), "y": y(d.SITE_EUI) };
                                          });
