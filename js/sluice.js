1dd294dede706e09c210f505dcfd303c624a2cf5
Daniel Bergey <bergey@alum.mit.edu>
2014-07-16 17:54:40 +0000

    like button example

--- js/sluice.js
+++ js/sluice.js
@@ -2,12 +2,26 @@
 
 var r = React.DOM;
 
+
+var LikeButton = React.createClass({
+    getInitialState: _.constant( {liked: false} ),
+    handleClick: function(event) {
+        this.setState( {liked: !this.state.liked} )
+    },
+    render: function() {
+        var text = this.state.liked ? 'like' : 'unlike';
+        return(r.p({onClick: this.handleClick}, 'You ', text, ' this.  Click to toggle.'))
+    }
+});
+
 var HelloWorld = React.createClass({
     render: function() {
         return r.div(
             {},
             r.p({}, 'Hello, ', this.props.name, '!'),
-            r.p({}, 'It is ', this.props.date.toTimeString()));
+            r.p({}, 'It is ', this.props.date.toTimeString()),
+            LikeButton()
+        );
     }});
 
 setInterval(function() {
