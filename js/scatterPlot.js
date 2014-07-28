2caa290bc80ef6b9f14a4bdbf69d368995e0c9ad
Daniel Bergey <bergey@alum.mit.edu>
2014-07-28 21:04:27 +0000

    improve axes, add y axis

--- js/scatterPlot.js
+++ js/scatterPlot.js
@@ -17,12 +17,6 @@ define(['react', 'd3', 'underscore'], function(React, d3, _) {
             }
     };
 
-    var defaultScale = function(data, w) {
-        return d3.scale.linear()
-            .domain([data.min, data.max])
-            .range([0, w]);
-    };
-
     var translate = function(x, y) {
         return 'translate(' + x.toString() + ',' + y.toString() + ')';
     };
@@ -55,6 +49,117 @@ define(['react', 'd3', 'underscore'], function(React, d3, _) {
         return _.range(min, max, step);
     };
 
+    // tickMark expects these props:
+    // scale: a function mapping the domain to SVG pixels : Num -> Num
+    // orient: one of 'top', 'bottom', 'right', 'left' : String
+    // tickLength: Num
+    // tickLabelSize: Num
+    // 
+    var tickMark = function(orient, scale, tickLength, labelSize) {
+        switch (orient) {
+           case "bottom":
+            {
+                return function(t) {
+                    return r.line({
+                        x1: scale(t),
+                        y1: 0,
+                        x2: scale(t),
+                        y2: tickLength,
+                        key: t    
+                    });
+                };
+            }
+            case "top":
+            {
+                return function(t) {
+                    return r.line({
+                        x1: scale(t),
+                        y1: 0,
+                        x2: scale(t),
+                        y2: -tickLength,
+                        key: t
+                    });
+                };
+            }
+            case "left":
+            {
+            return function(t) {
+                return r.line({
+                    x1: 0,
+                    y1: scale(t),
+                    x2: -tickLength,
+                    y2: scale(t),
+                    key: t
+                });
+            };
+            }
+            case "right":
+            {
+                return function(t) {
+                    return r.line({
+                        x1: 0,
+                        y1: scale(t),
+                        x2: tickLength,
+                        y2: scale(t),
+                        key: t
+                    });
+                };
+            }
+        }};
+
+    var tickText = function(orient, scale, tickLength, labelSize) {
+        var style;
+        var pos = tickLength + labelSize
+        switch (orient) {
+            case "bottom":
+            {
+                style = {"font-size": labelSize + "px;", "text-anchor": "center"};
+                return function(t) {
+                    return r.text({
+                        x: scale(t),
+                        y: pos,
+                        style: style
+                    }, t.toString());
+                };
+            }
+            case "top":
+            {
+                style = {"font-size": labelSize + "px;", "text-anchor": "center"};
+                return function(t) {
+                    return r.text({x: scale(t), y: -pos, style: style}, t.toString());
+                };
+            }
+            case "left":
+            {
+                style = {"font-size": labelSize + "px;", "text-anchor": "end"};
+                return function(t) {
+                    return r.text({x: -tickLength, y: scale(t) + labelSize/2, style: style}, t.toString());
+                };
+            }
+            case "right":
+            {
+                style = {"font-size": labelSize + "px;", "text-anchor": "start"};
+                return function(t) {
+                    return r.text({x: tickLength, y: scale(t) + labelSize/2, style: style}, t.toString());
+                };
+            }
+        }};
+
+    var axisLine = function(orient, scale, domain) {
+        switch(orient) {
+        case "bottom":
+        case "top":
+            {
+                return r.line({x1: scale(domain.min), x2: scale(domain.max), y1: 0, y2: 0});
+            }
+        case "right":
+        case "left":
+            {
+                return r.line({x1: 0, x2: 0, y1: scale(domain.min), y2: scale(domain.max)});
+            }
+        }
+    };
+ 
     // axis expects these props:
     // transform: a string in SVG transform format, relative to the parent : String
     // domain: The domain of values represented, with keys min, max : {}
@@ -64,6 +169,7 @@ define(['react', 'd3', 'underscore'], function(React, d3, _) {
     // orient: one of 'top', 'bottom', 'right', 'left' : String
     // TODO handle right and top cases!
     // tickLength: Num
+    // tickLabelSize: Num
     var axis = React.createClass({
         displayName: 'axis',
         render: function() {
@@ -74,36 +180,20 @@ define(['react', 'd3', 'underscore'], function(React, d3, _) {
             return r.g(
                 {transform: this.props.transform,
                 stroke: 'black'},
-                _.map(ticksList, function (t) {
-                    return r.line({
-                        x1: props.scale(t),
-                        y1: 0,
-                        x2: props.scale(t),
-                        y2: props.tickLength,
-                        key: t
-                    });
-                }),
-                r.line({
-                    x1: props.scale(props.domain.min),
-                    x2: props.scale(props.domain.max),
-                    y1: 0,
-                    y2: 0
-                }),
-                _.map(ticksList, function(t) {
-                    return r.text({
-                        x: props.scale(t),
-                        y: 12, // below tickLength
-                    }, t.toString());
-                })
+                _.map(ticksList, tickMark(props.orient, props.scale, props.tickLength, props.tickLabelSize)),
+                // axisLine
+                axisLine(props.orient, props.scale, props.domain),
+                _.map(ticksList, tickText(props.orient, props.scale, props.tickLength, props.tickLabelSize))
             );
         },
         getDefaultProps: function() {
             return {
-                ticks: function(domain, _scale) {
-                    return linspace(domain.min, domain.max, 5);
+                ticks: function(domain, scale) {
+                    return scale.ticks(5);
                 },
                 orient: 'bottom',
-                tickLength: 8
+                tickLength: 6,
+                tickLabelSize: 12
             };
         }});
 
@@ -143,6 +233,12 @@ define(['react', 'd3', 'underscore'], function(React, d3, _) {
                     transform: translate(props.margin.left, props.margin.top + viewHeight),
                     domain: extent(props.data, props.xKey),
                     scale: cx
+                }),
+                axis({
+                    transform: translate(props.margin.left, props.margin.top),
+                    domain: extent(props.data, props.yKey),
+                    scale: cy,
+                    orient: "left"
                 })
             );
         },
@@ -155,10 +251,16 @@ define(['react', 'd3', 'underscore'], function(React, d3, _) {
                     yKey: _.property('y'),
                     // xScale and yScale are functions
                     // xScale :: (value summary, viewport dim) -> x value -> output coördinate
-                    xScale: defaultScale,
-                    yScale: defaultScale,
+                    xScale: function(data, w) {
+                        return d3.scale.linear()
+                            .domain([data.min, data.max])
+                            .range([0, w]);
+                    },
+                    yScale: function(data, h) {
+                        return d3.scale.linear().domain([data.min, data.max]).range([h,0]);
+                    },
                     margin: {
-                        left: 20,
+                        left: 60,
                         right: 20,
                         top: 20,
                         bottom: 20
