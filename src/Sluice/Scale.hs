0be824e230d1742e99bc70f7e37302e4a567721e
Daniel Bergey <bergey@alum.mit.edu>
2014-10-16 14:19:09 +0000

    Default linear scale should use data domain

--- src/Sluice/Scale.hs
+++ src/Sluice/Scale.hs
@@ -5,6 +5,7 @@ module Sluice.Scale where
 
 import Control.Lens
 import Data.Default.Class
+import Numeric.Interval.Kaucher
 
 data Scale a = Scale {
     _forward :: a -> Double,
@@ -15,3 +16,10 @@ makeLenses ''Scale
 
 instance Default (Scale Double) where
     def = Scale id id
+
+-- TODO add some padding so no points are actually at 0 or 1
+-- TODO (harder) make `Interval -> Scale` functions take params, like padding %
+-- | The default linear scale maps the lower bound of the domain to 0,
+-- and the high end to 1.
+linearScale :: Interval Double -> Scale Double
+linearScale d = Scale (\a -> (a - inf d) / width d) (\b -> width d * b + inf d)
